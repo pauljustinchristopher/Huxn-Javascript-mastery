ba253b34b167b156c42af1d5fdbf2208391fa6ff
paul justin christopher <pauljustinchristopher@gmail.com>
2025-05-30 00:19:03 +0100

    I am try spread operator

---
+++ index.js
@@ -0,0 +1,6 @@
+// const multiply = (a, b=4) => a*b;
+// console.log(multiply(6));
+
+const people = ["Paul", "John", "Unique"]
+const printOut = ["Huxn", ...people, "Nanle"]
+console.log(printOut);
