98dc0ef3823785528b12cfb5a6277a28ba549bd6
paul justin christopher <pauljustinchristopher@gmail.com>
2025-06-09 03:34:25 +0100

    destructor

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
 // const multiply = (a, b=4) => a*b;
 // console.log(multiply(6));
 
-const people = ["Paul", "John", "Unique"]
-const printOut = ["Huxn", ...people, "Nanle"]
-console.log(printOut);
+// const people = ["Paul", "John", "Unique"]
+// const printOut = ["Huxn", ...people, "Nanle"]
+// console.log(printOut);
+
+
+// let arr = [1, 2, 3];
+// let arr2 = [4, 5];
+// let cloneArrArr2 = [...arr, ...arr2]
+
+// const user = {
+//     name: "Jen",
+//     age: 22
+// };
+// let cloneUser = {...user}
+
+// console.log(cloneArrArr2);
+// console.log(cloneUser);
+
+
+// user = (...userData) => console.log(userData);
+// user("Huxn", "Paul", "Justin")
+ 
+// const [a, ...b] = ["One", "two", "three", true, 12, ["one"]];
+// console.log(b);
+
+// const colors = ["red", "green", "blue", "yellow", "orange"];
+// const [one, two, three, ...four] = colors;
+
+// console.log(one);
+// console.log(two);
+// console.log(three)
+
+// const obj1 = {x:1, y:2};
+// const obj2 = {z:3};
+// console.log({...obj1, ...obj2});
+
+
+
+// let people =["huxn", "alex", "john"];
+// const allpeps = ["kumar", ...people, "Paul"];
+// console.log(allpeps);
+
+// let arr = [1, 2, 3];
+// let arr2 = [4, 5];
+// const arr3 = [...arr, ...arr2];
+// console.log(arr3);
+
+// const colors = ["red", "green", "blue", "yellow", "orange"];
+// const [color1, color2, color3] = colors;
+// console.log(color1, color2, color3);
+// console.log(color2);
+// console.log(color3);
+
+// const person = {
+//     name: "John Doe",
+//     age: 30,
+//     gender: "male",
+//     country: "USA",
+// };
+
+// const { name, age, country} = person;
+// console.log(name, age, country);
+
